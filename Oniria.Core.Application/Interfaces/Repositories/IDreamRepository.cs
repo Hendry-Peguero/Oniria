56cfe9c58a9a031bdc1db0fef9db3c32a9eae70d
Dennis Rubier Batista Perez <20230236@itla.edu.do>
2025-03-12 19:20:07 -0400

    Crear el repositorio que implemente los métodos:
    GetAllAsync
    
    GetByIdAsync
    
    CreateAsync

---
+++ Oniria.Core.Application/Interfaces/Repositories/IDreamRepository.cs
@@ -0,0 +1,13 @@
+﻿using Oniria.Core.Application.Interfaces.Repositories.Maintenance;
+using Oniria.Core.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Oniria.Core.Application.Interfaces.Repositories
+{
+    public interface IDreamRepository :
+    GetAllAsync<DreamEntity>,
+    GetByIdAsync<DreamEntity, string>
+    {
+        Task<DreamEntity> CreateAsync(DreamEntity dream);
+    }
+}
---
+++ Oniria.Infrastructure.Persistence/Repositories/SqlServer/Dream/DreamRepository.cs
@@ -0,0 +1,53 @@
+﻿using DreamHouse.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Oniria.Core.Application.Interfaces.Repositories;
+using Oniria.Core.Application.Interfaces.Repositories.Maintenance;
+using Oniria.Core.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Oniria.Infrastructure.Persistence.Repositories
+{
+
+    public class DreamRepository : IDreamRepository
+    {
+        private readonly ApplicationContext context;
+
+        public DreamRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<DreamEntity>> GetAllAsync()
+        {
+            return await this.context.Set<DreamEntity>().ToListAsync();
+        }
+
+        public async Task<DreamEntity> GetByIdAsync(string id)
+        {
+            return await this.context.Set<DreamEntity>().FindAsync(id);
+        }
+
+        public async Task<DreamEntity> CreateAsync(DreamEntity dream)
+        {
+            this.context.Set<DreamEntity>().Add(dream);
+            await this.context.SaveChangesAsync();
+            return dream;
+        }
+
+        Task<List<DreamEntity>> GetAllAsync<DreamEntity>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<DreamEntity?> GetByIdAsync<DreamEntity, string>.GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<DreamEntity> CreateAsync<DreamEntity>.CreateAsync(DreamEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
