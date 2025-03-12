e89e20054beac584aa1526397711bd87add4adc4
Dennis Rubier Batista Perez <20230236@itla.edu.do>
2025-03-12 16:29:44 -0400

    Creacion de OrganizationEntity

---
+++ Oniria.Core.Domain/Entities/OrganizationEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oniria.Core.Domain.Entities
+{
+  public class OrganizationEntity : BaseEntity 
+    {
+        String Name { get; set; }
+        String Address { get; set; }
+        String PhoneNumber  { get; set; }
+
+        String EmployeeOwnerld { get; set; }
+    }
+}
