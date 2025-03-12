4abbf00c982639945a24ac6cf4e471d23e358cca
Hendry <hpeguerovaldez@gmail.com>
2025-03-12 19:43:24 -0400

    Creacion de la entity Employee

---
+++ Oniria.Core.Domain/Entities/EmployeeEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Oniria.Core.Domain.Entities
+{
+    public class EmployeeEntity : BaseEntity
+    {
+        public String Dni {  get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public DateTime BornDate { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Address { get; set; }
+        public String UserId { get; set; }
+        public String OrganizationId { get; set; }
+    }
+}
