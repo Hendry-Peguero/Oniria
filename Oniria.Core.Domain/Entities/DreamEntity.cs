40e93d1190c7e9432973546a72825045a6ed487d
Dennis Rubier Batista Perez <20230236@itla.edu.do>
2025-03-12 16:53:46 -0400

    Creacion de DreamEntity

---
+++ Oniria.Core.Domain/Entities/DreamEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oniria.Core.Domain.Entities
+{
+  public class DreamEntity : BaseEntity
+    {
+        public String Tite { get; set; }
+        public String Prompt { get; set; }
+        public String PatientId { get; set; }
+        public String DreamAnalysisId { get; set; }
+    }
+}
