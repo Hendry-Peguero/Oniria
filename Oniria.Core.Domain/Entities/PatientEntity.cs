b8fd07b9349cb6304581ed1c463fc5525ff27db6
Adrian Ernesto Cuello Almonte <20230610@itla.edu.do>
2025-03-12 19:32:34 -0400

    Creacion del Entity Patient con cada uno de sus atributos.

---
+++ Oniria.Core.Domain/Entities/PatientEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oniria.Core.Domain.Entities
+{
+    public class PatientEntity: BaseEntity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BornDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string GenderId { get; set; }
+        public string UserId { get; set; }
+        public string OrganizationId { get; set; }
+    }
+}
