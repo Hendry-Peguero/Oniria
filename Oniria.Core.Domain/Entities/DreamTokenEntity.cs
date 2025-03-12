5feb32efff28cb5047410e9c7018df65a820f886
Dennis Rubier Batista Perez <20230236@itla.edu.do>
2025-03-12 16:39:04 -0400

    Creacion de DreamToken Entity

---
+++ Oniria.Core.Domain/Entities/DreamTokenEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oniria.Core.Domain.Entities
+{
+  public  class DreamTokenEntity : BaseEntity 
+    {
+        String PatientId { get; set; }
+        int? Amount { get; set; }
+        DateTime? RrefreshDate { get; set; }
+
+    }
+}
