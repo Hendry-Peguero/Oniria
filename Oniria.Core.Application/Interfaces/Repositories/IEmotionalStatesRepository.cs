0553161788dc54e48165c05e065eae813b740a34
Adrian Ernesto Cuello Almonte <20230610@itla.edu.do>
2025-03-12 22:13:05 -0400

    Creacion de repositorio EmotionalStatus y su interfaz

---
+++ Oniria.Core.Application/Interfaces/Repositories/IEmotionalStatesRepository.cs
@@ -0,0 +1,11 @@
+﻿using Oniria.Core.Application.Interfaces.Repositories.Maintenance;
+using Oniria.Core.Domain.Entities;
+
+namespace Oniria.Core.Application.Interfaces.Repositories
+{
+    public interface IEmotionalStatesRepository :
+        GetAllAsync<EmotionalStatesEntity>,
+        GetByIdAsync<EmotionalStatesEntity, string>
+    {
+    }
+}
