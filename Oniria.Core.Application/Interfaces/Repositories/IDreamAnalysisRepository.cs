d277aa0362b013e503f47f515877471954f3cc36
Dennis Rubier Batista Perez <20230236@itla.edu.do>
2025-03-12 22:49:14 -0400

    Creando DreamAnalysis Repository

---
+++ Oniria.Core.Application/Interfaces/Repositories/IDreamAnalysisRepository.cs
@@ -0,0 +1,19 @@
+﻿using Oniria.Core.Application.Interfaces.Repositories.Maintenance;
+using Oniria.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oniria.Core.Application.Interfaces.Repositories
+{
+    public interface IDreamAnalysisRepository:
+    GetAllAsync<DreamAnalysisEntity>,
+    GetByIdAsync<DreamAnalysisEntity, string>,
+    CreateAsync<DreamAnalysisEntity>
+    {
+
+    
+    }
+}
---
+++ Oniria.Infrastructure.Persistence/Repositories/SqlServer/DreamAnalysis/DreamAnalysisRepository.cs
@@ -0,0 +1,28 @@
+﻿using Oniria.Core.Application.Interfaces.Repositories;
+using Oniria.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oniria.Infrastructure.Persistence.Repositories.SqlServer.DreamAnalysis
+{
+    class DreamAnalysisRepository : IDreamAnalysisRepository
+    {
+        public Task<DreamAnalysisEntity> CreateAsync(DreamAnalysisEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<DreamAnalysisEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DreamAnalysisEntity?> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
