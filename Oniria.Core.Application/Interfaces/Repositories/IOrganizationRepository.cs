e58e7d04735765f87807193fa9b5605cf45571d1
Dennis Rubier Batista Perez <20230236@itla.edu.do>
2025-03-12 21:56:08 -0400

    Creacion de OrganizationRepository

--- Oniria.Core.Application/Interfaces/Repositories/IOrganizationRepository.cs
+++ Oniria.Core.Application/Interfaces/Repositories/IOrganizationRepository.cs
@@ -3,9 +3,15 @@ using Oniria.Core.Domain.Entities;
 
 namespace Oniria.Core.Application.Interfaces.Repositories
 {
-    public interface IGenderRepository :
+
+
+    public interface IOrganizationRepository :
     GetAllAsync<GenderEntity>,
-    GetByIdAsync<GenderEntity, string>
+    GetByIdAsync<GenderEntity, string>,
+    CreateAsync<GenderEntity>,
+    UpdateAsync<GenderEntity>,
+    DeleteAsync<GenderEntity>
+
     {
 
     }
