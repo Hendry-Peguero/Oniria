9a03a94a0af18e8be2a57ce8747aae40b75afd7f
Hendry <hpeguerovaldez@gmail.com>
2025-03-12 22:12:00 -0400

    Creacion de la entity MembershipBenefitRelation

---
+++ Oniria.Core.Domain/Entities/MembershipBenefitRelation.cs
@@ -0,0 +1,9 @@
+﻿namespace Oniria.Core.Domain.Entities
+{
+    public class MembershipBenefitRelation : BaseEntity
+    {
+        public bool Available { get; set; }
+        public string MembershipId { get; set; }
+        public string MembershipBenefitId { get; set; }
+    }
+}
