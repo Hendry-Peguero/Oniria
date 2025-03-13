d6f259d8d2a180b9700421b0f1fc70f8b6e61f80
Hendry <hpeguerovaldez@gmail.com>
2025-03-12 20:51:51 -0400

    Creacion de la entity MembershipEntity

---
+++ Oniria.Core.Domain/Entities/MembershipEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oniria.Core.Domain.Entities
+{
+    public class MembershipEntity : BaseEntity
+    {
+        public double Price { get; set; }
+        public String Description { get; set; }
+        public int DurationDays { get; set; }
+        public String MembershipCategoryId { get; set; }
+    }
+}
