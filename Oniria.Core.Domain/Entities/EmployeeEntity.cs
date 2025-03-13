8087038f2c9f8da0e87f1b75555737933ea5577f
Adrian Ernesto Cuello Almonte <20230610@itla.edu.do>
2025-03-12 21:54:32 -0400

    fix atributos de entidades

--- Oniria.Core.Domain/Entities/EmployeeEntity.cs
+++ Oniria.Core.Domain/Entities/EmployeeEntity.cs
@@ -2,13 +2,13 @@
 {
     public class EmployeeEntity : BaseEntity
     {
-        public String Dni {  get; set; }
-        public String Name { get; set; }
-        public String LastName { get; set; }
+        public string Dni {  get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
         public DateTime BornDate { get; set; }
-        public String PhoneNumber { get; set; }
-        public String Address { get; set; }
-        public String UserId { get; set; }
-        public String OrganizationId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string UserId { get; set; }
+        public string OrganizationId { get; set; }
     }
 }
