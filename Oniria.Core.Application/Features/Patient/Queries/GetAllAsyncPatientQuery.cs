380753b2097e23af00a0674d744cc19b30af03a2
Adrian Ernesto Cuello Almonte <20230610@itla.edu.do>
2025-04-03 21:20:28 -0400

    Creacion de los querries y sus funcionalidades de la entidad Patient

---
+++ Oniria.Core.Application/Features/Patient/Queries/GetAllAsyncPatientQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Oniria.Core.Application.Features.Base;
+using Oniria.Core.Domain.Entities;
+using Oniria.Core.Domain.Interfaces.Repositories;
+namespace Oniria.Core.Application.Features.Patient.Queries
+{
+    public class GetAllAsyncPatientQuery : IRequest<OperationResult<List<PatientEntity>>> { }
+    public class GetAllAsyncPatientHandler : IRequestHandler<GetAllAsyncPatientQuery, OperationResult<List<PatientEntity>>>
+    {
+        private readonly IPatientRepository patientRepository;
+        public GetAllAsyncPatientHandler(IPatientRepository patientRepository)
+        {
+            this.patientRepository = patientRepository;
+        }
+        public async Task<OperationResult<List<PatientEntity>>> Handle(GetAllAsyncPatientQuery request, CancellationToken cancellationToken)
+        {
+            var result = new OperationResult<List<PatientEntity>>();
+            result.Data = await patientRepository.GetAllAsync();
+            return result;
+        }
+    }
+}
