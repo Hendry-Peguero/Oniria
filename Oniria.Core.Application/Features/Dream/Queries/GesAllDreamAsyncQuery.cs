ac8c12d728484ae0d0ef49e886b8544aba20fe3a
Dennis Rubier Batista Perez <dennisrubierbatistaperez@gmail.com>
2025-04-08 14:42:57 -0700

    Creacion de Queries Dream

---
+++ Oniria.Core.Application/Features/Dream/Queries/GesAllDreamAsyncQuery.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Oniria.Core.Application.Features.Base;
+using Oniria.Core.Domain.Entities;
+using Oniria.Core.Domain.Interfaces.Repositories;
+
+namespace Oniria.Core.Application.Features.Dream.Queries
+{
+    public class GetAllDreamAsyncQuery : IRequest<OperationResult<List<DreamEntity>>> { }
+
+    public class GetAllDreamAsyncQueryHandler : IRequestHandler<GetAllDreamAsyncQuery, OperationResult<List<DreamEntity>>>
+    {
+        private readonly IDreamRepository dreamRepository;
+
+        public GetAllDreamAsyncQueryHandler(IDreamRepository dreamRepository)
+        {
+            this.dreamRepository = dreamRepository;
+        }
+
+        public async Task<OperationResult<List<DreamEntity>>> Handle(GetAllDreamAsyncQuery request, CancellationToken cancellationToken)
+        {
+            var result = OperationResult<List<DreamEntity>>.Create();
+            result.Data = await dreamRepository.GetAllAsync();
+            return result;
+        }
+    }
+}
---
+++ Oniria.Core.Dtos/Dream/Response/DreamResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Oniria.Core.Dtos.Dream.Response
+{
+
+    public class DreamResponse
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Prompt { get; set; }
+        public string PatientId { get; set; }
+        public string DreamAnalysisId { get; set; }
+    }
+
+}
