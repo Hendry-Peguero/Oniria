d14c4a38ee7580ad691be96faa834f4321c87d43
Adrian Ernesto Cuello Almonte <20230610@itla.edu.do>
2025-04-09 14:38:00 -0400

    Fix validación EmotionalStatesEntityId y EmotionalStateId.

--- Oniria.Core.Application/Features/DreamAnalysis/Commands/CreateDreamAnalisysAsyncCommand.cs
+++ Oniria.Core.Application/Features/DreamAnalysis/Commands/CreateDreamAnalisysAsyncCommand.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Oniria.Core.Application.Features.Base;
 using Oniria.Core.Application.Features.DreamAnalysis;
+using Oniria.Core.Application.Features.EmotionalStates.Queries;
 using Oniria.Core.Domain.Entities;
 using Oniria.Core.Domain.Interfaces.Repositories;
 using Oniria.Core.Dtos.DreamAnalsys.Request;
@@ -17,20 +18,34 @@ namespace Oniria.Core.Application.Features.DreamAnalysis.Commands
     {
         private readonly IDreamAnalysisRepository dreamAnalysisRepository;
 
+        private readonly IMediator mediator;
+
         private readonly IMapper mapper;
 
-        public CreateDreamAnalysisAsyncCommandHandler(IDreamAnalysisRepository dreamAnalysisRepository, IMapper mapper)
+        public CreateDreamAnalysisAsyncCommandHandler(IDreamAnalysisRepository dreamAnalysisRepository, IMapper mapper, IMediator mediator)
         {
             this.dreamAnalysisRepository = dreamAnalysisRepository;
 
+            this.mediator = mediator;
+
             this.mapper = mapper;
         }
+
 
         public async Task<OperationResult<DreamAnalysisEntity>> Handle(CreateDreamAnalisysAsyncCommand command, CancellationToken cancellationToken)
         {
             var result = OperationResult<DreamAnalysisEntity>.Create();
 
             var request = command.Request;
+
+            var emotionalStatesResult = await mediator.Send(new GetEmotionalStatesByIdAsyncQuery { Id = request.EmotionalStateId });
+
+            if (!emotionalStatesResult.IsSuccess)
+            {
+                result.AddError(emotionalStatesResult);
+
+                return result;
+            }
 
             var dreamAnalysisToCreate = mapper.Map<DreamAnalysisEntity>(request);
 
