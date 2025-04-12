62c85635cdc18500cb9db49ac3a1c0a2e4eaa169
Dennis Rubier Batista Perez <dennisrubierbatistaperez@gmail.com>
2025-04-11 17:46:27 -0700

    Creacion de controllar de Organization

---
+++ Oniria/Controllers/OrganizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Oniria.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
