9eddba4a1dc67373a09d905fff0ffb19344beb95
Dennis Rubier Batista Perez <dennisrubierbatistaperez@gmail.com>
2025-04-11 17:14:42 -0700

    Creacion de controlladores de Dream

---
+++ Oniria/Controllers/DreamController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oniria.Controllers.Commons;
+using Oniria.Core.Application.Features.Dream.Commands;
+using Oniria.Core.Dtos.Dream.Request;
+using System.Threading.Tasks;
+
+namespace Oniria.Controllers
+{
+    public class DreamController : BaseController
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        public async Task<IActionResult> Create(CreateDreamRequest request)
+        {
+            if (!ModelState.IsValid)
+                return View(request);
+
+            var result = await Mediator.Send(new CreateDreamAsyncCommand { Request = request });
+
+            if (!result.IsSuccess)
+            {
+                foreach (var message in result.Messages)
+                    ModelState.AddModelError(string.Empty, message);
+
+                return View(request);
+            }
+
+            return RedirectToAction("Index", "Dream");
+        }
+    }
+}
