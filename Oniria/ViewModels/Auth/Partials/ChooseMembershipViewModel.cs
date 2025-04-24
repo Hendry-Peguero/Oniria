a98d027e865ff29453191c3cb65595fed47e4072
Sedtt <moisesgironarias@gmail.com>
2025-04-24 18:41:15 -0400

    correccion de cosita

---
+++ Oniria/ViewModels/Auth/Partials/ChooseMembershipViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Oniria.Core.Domain.Entities;
+
+namespace Oniria.ViewModels.Auth.Partials
+{
+    public class ChooseMembershipViewModel
+    {
+        public string InputKey { get; set; }
+        public List<MembershipEntity>? Memberships { get; set; }
+    }
+}
