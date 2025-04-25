0b47e370c27a7db7a23f3ead0f6c8b41efb68cc0
Sedtt <moisesgironarias@gmail.com>
2025-04-25 14:07:59 -0400

    api base

---
+++ OniriaApi/Extensions/AppExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace OniriaApi.Extensions
+{
+    public static class AppExtensions
+    {
+        public static void UseSwaggerExtension(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "StockApp API");
+                options.DefaultModelRendering(ModelRendering.Model);
+            });
+        }
+    }
+}
